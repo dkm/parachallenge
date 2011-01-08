5f43e2048ebc336c5c4faa310823334e0e11219a
Marc <dkm@kataplop.net>
2011-01-08 16:52:58 +0100

    a flight includes taking-off and landing... in most case =)

--- parachallenge/__init__.py
+++ parachallenge/__init__.py
@@ -8,19 +8,24 @@ class Waypoint:
         self.name = name
 
     def __str__(self):
-        return "Balise:" + self.lat + "," + self.lon + self.name
+        return str(self.lat) + ", " + str(self.lon)+ " | " + self.name
         
 class Cross:
-    def __init__(self,name,difficulty,description,waypoints):
+    def __init__(self, name, difficulty, description, 
+                 takeoff, landing, waypoints):
         self.name = name
         self.difficulty = difficulty
         self.description = description
         self.waypoints = waypoints
+        self.takeoff = takeoff
+        self.landing = landing
 
     def __str__(self):
         retstr =  "Name:" + self.name + "/"
         retstr += "Difficulté:" + self.difficulty + "/"
         retstr += "Description:" + self.description + "/"
+        retstr += "Deco:" + str(self.takeoff)
         retstr += "Balises:"+ "/".join([str(x) for x in self.waypoints])
+        retstr += "Atterro:" + str(self.landing)
         return retstr
 
