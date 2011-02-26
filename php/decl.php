7fe4eb7d1cbde7d5de8e6e423bc52b62c3c23bbe
Marc Poulhiès <dkm@kataplop.net>
2011-02-26 14:18:12 +0100

    more logic in php (check basic validity of data) add datepicker widget for date input check returned value when async POST is used. check valid data is sent (no null/empty values)

--- php/decl.php
+++ php/decl.php
@@ -3,12 +3,33 @@
 header("Content-Type:  application/json");
 
 $login = $_POST['login'];
+$password = $_POST['password'];
 $cross = $_POST['cross'];
 $lastb = $_POST['lastwpt'];
+$date = $_POST['date'];
+
+$valid = true;
+
+/*
+ * check login/pass
+ */
+/* if ($login == "failure"){ */
+/*   echo json_encode(array("result"=>false, "reason"=>"bad login")); */
+/*   return; */
+/*  } */
+
+/*
+ * check valid data
+ */
+
+if (! $valid){
+  echo json_encode(array("result"=>false, "reason"=>"bad data"));
+  return;
+}
 
 $str = "[declaration] \n" .
        "pilot : " . $login . "\n" .
-       "date : " . "12/12/12 \n" .
+       "date : " . $date . "\n" .
        "cross : " . $cross . "\n" .
        "last_balise : " . $lastb . "\n";
 
@@ -20,6 +41,6 @@ $fname = tempnam("declarations", $pref);
 $handle = fopen($fname, "w");
 fwrite($handle, $str);
 fclose($handle);
-echo json_encode(array("result"=>true,"file"=>"$pref"));
+echo json_encode(array("result"=>true,"file"=>"$fname"));
 /*header('Location: index.html');*/
 ?>
